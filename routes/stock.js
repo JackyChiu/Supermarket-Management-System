7edcedda7cd58ffb7d77b9f2a21f343ffb85f8fd
Jacky Chiu <jacky.chiu888@gmail.com>
2016-10-15 23:12:53 -0400

    Added inital stock route, and model

---
+++ routes/stock.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Stock = require('../models/StockModel.js');
+
+const router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+router.get('/', (req, res) => {
+    Stock.find().lean().then((stockList) => {
+        console.log(stockList);
+        res.send(stockList);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    });
+});
+
+router.post('/', (req, res) => {
+    console.log('=======Object Recieved======'); 
+    console.log(req.body);
+
+    const product = new Stock({
+        'product': req.body.product,
+        'expiryDate': req.body.expiryDate,
+        'quantity': req.body.quantity
+    });
+
+    product.save().then((result) => {
+        console.log(`Saved ${JSON.stringify(result, null, 4)});
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500);
+    });
+});
+
+router.put('/', (req, res) => {
+    console.log(req.body); 
+    Stock.findById(req._id).then((stock) => {
+        stock.product = req.body.product;
+        stock.expiryDate = req.body.expiryDate;
+        stock.quantity = req.body.quantity;
+        return stock.save();
+    }).then((result) => {
+        console.log(result);
+        res.send(result);  
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Stock.remove({
+        'id': req.params._id
+    }).then((result) => {
+        console.log(`Deleted ${result}`);
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    }); 
+});
+
+module.exports = router;
