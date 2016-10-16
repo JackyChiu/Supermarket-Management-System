61c51999924ac5aead3e1425facd08f76481630f
kirinrastogi <rastogikirin@gmail.com>
2016-10-15 23:24:34 -0400

    Add volunteer schema

---
+++ models/VolunteerSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const volunteerSchema = new Schema({
+    'name': String,
+    'monday': {
+		start: String,
+		end: String},
+	'tuesday': {
+		start: String,
+		end: String},
+	'wednesday': {
+		start: String,
+		end: String},
+	'thursday': {
+		start: String,
+		end: String},
+	'friday': {
+		start: String,
+		end: String},
+	'saturday': {
+		start: String,
+		end: String},
+	'sunday': {
+		start: String,
+		end: String}
+}, { versionKey: 'version' });
+
+const Stock = mongoose.model('Volunteer', volunteerSchema, 'Volunteer');
+
+module.exports = Volunteer;
