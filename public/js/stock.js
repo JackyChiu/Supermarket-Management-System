49cc8350181cf8e92d3b81f435f938dcfbb65a6e
vikrambombhi <vbombhi@gmail.com>
2016-10-16 02:16:30 -0400

    hope this works

--- public/js/stock.js
+++ public/js/stock.js
@@ -18,6 +18,8 @@ function stockController($http) {
         });
     }
 
+    pieChart();
+
     stockCtrl.postStock = () => {
         console.log(stockCtrl.stock);
         $http.post('/stock', stockCtrl.stock).then((res) => {
@@ -42,4 +44,43 @@ function stockController($http) {
             console.log(error);
         });
     }
+
+    function pieChart(){
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      getStock();
+      let mostPopular = [];
+      mostPopular.push(stockList[0]);
+      mostPopular.push(stockList[1]);
+      mostPopular.push(stockList[2]);
+      mostPopular.push(stockList[3]);
+      mostPopular.push(stockList[4]);
+      for ( let i = 5; i < stockList.length; i++) {
+            for(let j = 0; j<mostPopular.length; j++ ) {
+            if (stockList[i].orders > mostPopular[j].orders){
+                mostPopular[j] = stockList[i];
+                j = mostPopular.length;
+            }
+            }
+        }
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Item', 'Sold number'],
+          [mostPopular[0].product,     mostPopular[0].orders],
+          [mostPopular[1].product,     mostPopular[1].orders],
+          [mostPopular[2].product,     mostPopular[2].orders],
+          [mostPopular[3].product,     mostPopular[3].orders],
+          [mostPopular[4].product,     mostPopular[4].orders]
+        ]);
+
+        var options = {
+          title: 'Popular Item'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+      }
 }
