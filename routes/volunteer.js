82d7f57050b175a90a2f9a7faca89231b4970480
kirinrastogi <rastogikirin@gmail.com>
2016-10-16 01:44:10 -0400

    Update routes for volunteer, broken put

--- routes/volunteer.js
+++ routes/volunteer.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    console.log('=======Object Recieved======'); 
+    console.log('=======Object Recieved======');
     console.log(req.body);
 
     const person = new Volunteer({
@@ -52,7 +52,7 @@ router.post('/', (req, res) => {
 	});
 
     person.save().then((result) => {
-        console.log(`Saved ${JSON.stringify(result, null, 4)});
+        console.log('Saved ${JSON.stringify(result, null, 4)}');
         res.send(result);
     }).catch((err) => {
         console.log(err);
@@ -61,36 +61,37 @@ router.post('/', (req, res) => {
 });
 
 router.put('/', (req, res) => {
-    console.log(req.body); 
+    console.log(req.body);
     Volunteer.findById(req._id).then((volunteer) => {
 		// TODO
-			
-   		 'name'= req.body.name,
-    	'monday': {
-			start= req.body.monday.start,
-			end= req.body.monday.end},
-		'tuesday': {
-			start= req.body.tuesday.start,
-			end= req.body.tuesday.end},
-		'wednesday': {
-			start= req.body.wednesday.start,
-			end= req.body.wednesday.end},
-		'thursday': {
-			start= req.body.thursday.start,
-			end= req.body.thursday.end},
-		'friday': {
-			start= req.body.friday.start,
-			end= req.body.friday.end},
-		'saturday': {
-			start= req.body.saturday.start,
-			end= req.body.saturday.end},
-		'sunday': {
-			start= req.body.sunday.start,
-			end= req.body.sunday.end}
+   		volunteer.
+		 volunteer.name = req.body.name;
+   		volunteer. monday = {
+   			volunteer.monday.start = req.body.monday.start;
+			  volunteer.monday.end = req.body.monday.end
+      };
+		 volunteer.tuesday = {
+			volunteer.start = req.body.tuesday.start;
+			volunteer.end = req.body.tuesday.end};
+		 volunteer.wednesday = {
+			volunteer.start= req.body.wednesday.start;
+			volunteer.end= req.body.wednesday.end};
+		 volunteer.thursday = {
+			volunteer.start= req.body.thursday.start;
+			volunteer.end= req.body.thursday.end};
+		 volunteer.friday = {
+			volunteer.start= req.body.friday.start;
+			volunteer.end= req.body.friday.end};
+		 volunteer.saturday = {
+			volunteer.start= req.body.saturday.start;
+			volunteer.end= req.body.saturday.end};
+		 volunteer.sunday = {
+			volunteer.start= req.body.sunday.start;
+			volunteer.end= req.body.sunday.end}
 		return volunteer.save();
     }).then((result) => {
         console.log(result);
-        res.send(result);  
+        res.send(result);
     }).catch((err) => {
         console.log(err);
         res.status(500).end();
@@ -106,7 +107,7 @@ router.delete('/:id', (req, res) => {
     }).catch((err) => {
         console.log(err);
         res.status(500).end();
-    }); 
+    });
 });
 
 module.exports = router;
