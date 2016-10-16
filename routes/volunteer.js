4bade71d723b25b153d4875a2635da702dbb7843
Jacky Chiu <jacky.chiu888@gmail.com>
2016-10-16 03:35:57 -0400

    Volunteer right getting called

--- routes/volunteer.js
+++ routes/volunteer.js
@@ -27,32 +27,39 @@ router.post('/', (req, res) => {
     console.log(req.body);
 
     const person = new Volunteer({
-    'name': req.body.name,
-    'monday': {
-		'start': req.body.monday.start,
-		'end': req.body.monday.end},
-	'tuesday': {
-		'start': req.body.tuesday.start,
-		'end': req.body.tuesday.end},
-	'wednesday': {
-		'start': req.body.wednesday.start,
-		'end': req.body.wednesday.end},
-	'thursday': {
-		'start': req.body.thursday.start,
-		'end': req.body.thursday.end},
-	'friday': {
-		'start': req.body.friday.start,
-		'end': req.body.friday.end},
-	'saturday': {
-		'start': req.body.saturday.start,
-		'end': req.body.saturday.end},
-	'sunday': {
-		'start': req.body.sunday.start,
-		'end': req.body.sunday.end}
+        'name': req.body.name,
+        'monday': {
+            'start': req.body.monday.start,
+            'end': req.body.monday.end
+        },
+        'tuesday': {
+            'start': req.body.tuesday.start,
+            'end': req.body.tuesday.end
+        },
+        'wednesday': {
+            'start': req.body.wednesday.start,
+            'end': req.body.wednesday.end
+        },
+        'thursday': {
+            'start': req.body.thursday.start,
+            'end': req.body.thursday.end
+        },
+        'friday': {
+            'start': req.body.friday.start,
+            'end': req.body.friday.end
+        },
+        'saturday': {
+            'start': req.body.saturday.start,
+            'end': req.body.saturday.end
+        },
+        'sunday': {
+            'start': req.body.sunday.start,
+            'end': req.body.sunday.end
+        }
 	});
 
     person.save().then((result) => {
-        console.log('Saved ${JSON.stringify(result, null, 4)}');
+        console.log(`Saved ${JSON.stringify(result, null, 4)}`);
         res.send(result);
     }).catch((err) => {
         console.log(err);
@@ -99,7 +106,7 @@ router.put('/', (req, res) => {
 */
 router.delete('/:id', (req, res) => {
     Volunteer.remove({
-        'id': req.params._id
+        '_id': req.params.id
     }).then((result) => {
         console.log(`Deleted ${result}`);
         res.send(result);
