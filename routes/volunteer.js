73a77118297dc5cb5989b96b1cac8c5f4fa3b8af
Jacky Chiu <jacky.chiu888@gmail.com>
2016-10-19 12:44:12 -0400

    Route cleanup

--- routes/volunteer.js
+++ routes/volunteer.js
@@ -62,44 +62,7 @@ router.post('/', (req, res) => {
         res.status(500);
     });
 });
-/*
-router.put('/', (req, res) => {
-    console.log(req.body);
-    Volunteer.findById(req._id).then((volunteer) => {
-		// TODO
-		 volunteer.name = req.body.name;
-   		volunteer.monday = {
-   			volunteer.monday.start = req.body.monday.start;
-			  volunteer.monday.end = req.body.monday.end
-      };
-		 volunteer.tuesday = {
-			volunteer.start = req.body.tuesday.start;
-			volunteer.end = req.body.tuesday.end};
-		 volunteer.wednesday = {
-			volunteer.start= req.body.wednesday.start;
-			volunteer.end= req.body.wednesday.end};
-		 volunteer.thursday = {
-			volunteer.start= req.body.thursday.start;
-			volunteer.end= req.body.thursday.end};
-		 volunteer.friday = {
-			volunteer.start= req.body.friday.start;
-			volunteer.end= req.body.friday.end};
-		 volunteer.saturday = {
-			volunteer.start= req.body.saturday.start;
-			volunteer.end= req.body.saturday.end};
-		 volunteer.sunday = {
-			volunteer.start= req.body.sunday.start;
-			volunteer.end= req.body.sunday.end}
-		return volunteer.save();
-    }).then((result) => {
-        console.log(result);
-        res.send(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).end();
-    });
-});
-*/
+
 router.delete('/:id', (req, res) => {
     Volunteer.remove({
         '_id': req.params.id
